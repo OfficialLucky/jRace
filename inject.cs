c480d2813fede4b66176c0aee9dd5bc4568b12b2
OfficialLucky <Jamesash1@live.com>
2017-10-21 18:26:18 -0400

    Inject Add

---
+++ inject.cs
@@ -0,0 +1,13 @@
+using RestSharp;
+using jRace.PrivateClass.Main;
+Namespace Inject {
+
+  public class Inject {
+  
+  privatef = new jRace.PrivateClass.Main(); 
+  void(){
+  privatef.Send();
+  return;
+}
+  }
+}
