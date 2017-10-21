a6c8dab436f9605b4d01cb399b3d6685d44440ce
OfficialLucky <Jamesash1@live.com>
2017-10-21 16:01:57 -0400

    Added Google API Source

--- jRace.js
+++ jRace.js
@@ -1,4 +1,5 @@
 var jq = document.createElement('script');
+jq.src = "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
 (function($)
 {
